6707a1bcdd40072e21dcbaab2f9f01e6c15abef6
neerajdembla-gyg <neeraj.dembla@getyourguide.com>
2025-01-13 14:26:24 +0100

    making bug harder to find by introducing multiple data source

--- vue-frontend/src/services/activity.ts
+++ vue-frontend/src/services/activity.ts
@@ -1,14 +1,15 @@
 export default {
     async getActivities() {
       try {
-        const response = await fetch('/activities.json');
+        const dataSource = Math.random() > 0.5 ? '/activities.json' : '/activitiesV2.json';
+        const response = await fetch(dataSource);
         if (!response.ok) {
           throw new Error('Failed to fetch activities');
         }
         const data = await response.json();
         await new Promise(resolve => setTimeout(resolve, Math.random() * 2000));
         let activities = [];
-        if (Math.random() > 0.5) {
+        if (data.some(activity => activity.supplierId > 200)) {
           activities = data;
         }
         return  activities;
