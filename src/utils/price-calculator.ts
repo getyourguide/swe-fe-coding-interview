e078f52b4d352ac42b8b95a6caf4e0803038e984
neerajdembla-gyg <neeraj.dembla@getyourguide.com>
2025-01-17 10:33:33 +0100

    adding task to calculate discounted price

--- src/utils/price-calculator.ts
+++ src/utils/price-calculator.ts
@@ -1,32 +1,28 @@
-import { CartItem } from "@/types/cart-item";
+import type { CartItem } from "@/types/cart-item";
 
-export function calculateCartTotal(cartItems: CartItem[], tax?: number): string {
+export function calculateCartTotal(cartItems: CartItem[], governmentTax?: number): string {
   return "";
 }
 
 /*
-  example usage
+  Example usage
   const items = [
     {
-    price: 19.99,
+    priceBeforeTax: 19.99,
     name: "Product 1",
     },
     {
-    price: 9.99,
+    priceBeforeTax: 9.99,
     name: "Product 2",
     },
     {
-    price: 9.99,
-    name: "Product 2",
-    },
-    {
-    price: 5.99,
+    priceBeforeTax: 5.99,
     name: "Product 4",
     },
   ];
   const tax = 0.07;
   const total = calculateCartTotal(items, tax);
-  cosole.log(total); // 36.04
+  cosole.log(total); // 36.18
   */
 
 
