3c804e92ad6e860cf3d7593d6b42cd5bd0262366
neerajdembla-gyg <neeraj.dembla@getyourguide.com>
2025-01-20 10:40:13 +0100

    update calculate total task with correct return type

--- src/types/cart-item.ts
+++ src/types/cart-item.ts
@@ -1,4 +1,4 @@
 export interface CartItem {
   name: string;
-  price: number;
+  priceBeforeTax: number;
 }
--- src/utils/price-calculator.ts
+++ src/utils/price-calculator.ts
@@ -1,11 +1,7 @@
 import type { CartItem } from "@/types/cart-item";
 
-export function calculateCartTotal(cartItems: CartItem[], governmentTax?: number): string {
-  return "";
-}
-
 /*
-  Example usage
+  Example usage 1, 
   const items = [
     {
     priceBeforeTax: 19.99,
@@ -22,7 +18,41 @@ export function calculateCartTotal(cartItems: CartItem[], governmentTax?: number
   ];
   const tax = 0.07;
   const total = calculateCartTotal(items, tax);
-  cosole.log(total); // 36.18
+  cosole.log(total); // { status: "success", total: "36.18" }
+
+
+  --------------------------------------------
+  Example usage 2,
+  const items = [
+    {
+    priceBeforeTax: 19.99,
+    name: "Product 1",
+    },
+    {
+    priceBeforeTax: 9.99,
+    name: "Product 2",
+    },
+    {
+    priceBeforeTax: 9.99,
+    name: "Product 2",
+    },
+    {
+    priceBeforeTax: 5.99,
+    name: "Product 4",
+    },
+  ];
+  const tax = 0.07;
+  const total = calculateCartTotal(items, tax);
+  cosole.log(total); // { status: "error", message: "Cart has duplicate items" }
   */
+  
+  type CartTotalResult = 
+  | { status: "success"; total: string }
+  | { status: "error"; message: string };
 
-
+export function calculateCartTotal(cartItems: CartItem[], tax?: number): CartTotalResult {
+  return {
+    status: "success",
+    total: "0.00",
+  };
+}
