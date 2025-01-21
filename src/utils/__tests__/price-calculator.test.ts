75013110359f9bf5ce77eeb21fb7014f98602551
Victor Persien <victor.persien@getyourguide.com>
2025-01-21 14:41:06 +0100

    fix property name

--- src/utils/__tests__/price-calculator.test.ts
+++ src/utils/__tests__/price-calculator.test.ts
@@ -3,6 +3,6 @@ import { calculateCartTotal } from "../price-calculator";
 describe("calculateCartTotal()", () => {
   it("calculates the total of a number of cart items", () => {
     const total = calculateCartTotal([], 0.07);
-    expect(total).toEqual({ status: "success", value: "0.00" });
+    expect(total).toEqual({ status: "success", total: "0.00" });
   });
 });
